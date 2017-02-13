71b4bc116290ca00bc52dc71a09b31cf112f3479
Adrien Boutigny <adrien.boutigny@gmail.com>
2017-02-13 16:49:19 +0100

    Supprimé des fichiers qui trainaient

--- resources/library/view/layout/combo_page.php
+++
@@ -1,9 +0,0 @@
-<p id="character">Character : <?php echo $character; ?></p>
-<p id="description">Description : <?php echo $description; ?></p>
-<ul id="combo">
-<?php
-foreach($moveList as $move){
-    echo "<li>".$move."</li>\n";
-}
-?>
-</ul>
