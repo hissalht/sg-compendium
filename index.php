cc19f04636793f3cfa0a7d235899230033f7de70
Adrien Boutigny <adrien.boutigny@gmail.com>
2017-02-13 16:16:26 +0100

    Supprimé anciennne structure

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-set_include_path("./src");
-require_once("Router.php");
-
-$router = new Router();
-$router->main();
-?>
--- src/model/ComboStorage.php
+++
@@ -1,7 +0,0 @@
-<?php
-interface ComboStorage {
-    public function getCombo($id);
-    public function getAllCombos();
-    public function addCombo(Combo $combo);
-}
-?>
